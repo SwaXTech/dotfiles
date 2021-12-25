65cf0ed1719a989f5aa2354e7f9ff55c8b74c2d2
= <aleeherasimiuk@gmail.com>
2021-12-25 18:15:50 -0300

    Created shell execution function using subprocess and shlex

---
+++ config.py
@@ -0,0 +1,10 @@
+import subprocess
+import shlex
+
+def exec(command):
+    splitted_command = shlex.split(command)
+    process = subprocess.run(splitted_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    stdout = process.stdout
+    stderr = process.stderr
+    returncode = process.returncode
+    return stdout, stderr, returncode
