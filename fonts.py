24eea892ccca02ff52063e42bcbd87b6afec4596
alee <aleeherasimiuk@gmail.com>
2021-12-26 16:25:46 -0300

    Update main.py

--- fonts.py
+++ fonts.py
@@ -4,11 +4,7 @@ from shell import exec
 
 def install_fonts():
     print('Installing JetBrains Mono Font')
-    _, _, status = exec('cp -rvT ./fonts/jetbrains_mono/ttf/ /usr/share/fonts/TTF')
-    print(status)
+    _, _, status1 = exec('cp -rvT ./fonts/jetbrains_mono/ttf/ /usr/share/fonts/TTF')
     print('Installing Meslo Nerd Font')
-    stdout, stderr, status = exec('cp -rvT ./fonts/meslo_nerd_font/ /usr/share/fonts/TTF')
-    print(status)
-    print(stderr)
-
-install_fonts()
+    stdout, stderr, status2 = exec('cp -rvT ./fonts/meslo_nerd_font/ /usr/share/fonts/TTF')
+    return !status1 && !status2
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from btrfs_snapshots import make_snapshot
+from fonts import install_fonts
 import sys
 
 snapshot = make_snapshot()
 
 if not snapshot:
-    sys.exit(1);
+    sys.exit(1)
 
+fonts = install_fonts()
 
+if not fonts:
+    sys.exit(1)
 
