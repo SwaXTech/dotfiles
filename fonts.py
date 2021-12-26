2d95dc8bb72cdbdda5dc1003256f1e68ed2583bc
alee <aleeherasimiuk@gmail.com>
2021-12-26 16:21:24 -0300

    Installing fonts to system

--- fonts.py
+++ fonts.py
@@ -1,13 +1,14 @@
-#!/bin/python
+#!/usr/bin/env python
 
 from shell import exec
 
 def install_fonts():
     print('Installing JetBrains Mono Font')
-    _, _, status = exec('cp -r ./fonts/jetbrains_mono/ttf /usr/share/fonts/TTF')
+    _, _, status = exec('cp -rvT ./fonts/jetbrains_mono/ttf/ /usr/share/fonts/TTF')
     print(status)
     print('Installing Meslo Nerd Font')
-    _, _, status = exec('cp -r ./fonts/meslo_nerd_font /usr/share/fonts/TTF')
+    stdout, stderr, status = exec('cp -rvT ./fonts/meslo_nerd_font/ /usr/share/fonts/TTF')
     print(status)
+    print(stderr)
 
 install_fonts()
