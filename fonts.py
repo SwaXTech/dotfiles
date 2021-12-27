5b8e0edc6772d7adf19f78f84c6f4791071fabdf
alee <aleeherasimiuk@gmail.com>
2021-12-27 01:11:04 -0300

    Bugfixes on font file

--- fonts.py
+++ fonts.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from shell import exec
+from shell import exec_command
 
 def install_fonts():
     print('Installing JetBrains Mono Font')
-    _, _, status1 = exec('cp -rvT ./fonts/jetbrains_mono/ttf/ /usr/share/fonts/TTF')
+    _, _, status1 = exec_command('cp -rvT ./fonts/jetbrains_mono/ttf/ /usr/share/fonts/TTF')
     print('Installing Meslo Nerd Font')
-    stdout, stderr, status2 = exec('cp -rvT ./fonts/meslo_nerd_font/ /usr/share/fonts/TTF')
-    return !status1 && !status2
+    stdout, stderr, status2 = exec_command('cp -rvT ./fonts/meslo_nerd_font/ /usr/share/fonts/TTF')
+    print('All fonts installed. Remember restart terminal to refresh')
+    return not status1 and not status2
