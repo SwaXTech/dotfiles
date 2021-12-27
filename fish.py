650ca1cadb296f2aab525bded1e3ff6f781dd85c
alee <aleeherasimiuk@gmail.com>
2021-12-27 01:05:47 -0300

    Updated main.py && added return values

--- fish.py
+++ fish.py
@@ -23,19 +23,24 @@ def install_fish():
 
     print("Installing OhMyFish!")
     stdout, stderr, status = exec_command(f'fish ohmyfish.sh --noninteractive --yes')
+    if status != 0:
+        return False
     #print(stdout, stderr, status)
     stdout, stderr, status = exec_command(f'rm ohmyfish.sh')
+    if status != 0:
+        return False
     #print(stdout, stderr, status)
 
     print("Changing Shell")
     stdout, stderr, status = exec_command(f'chsh {user} -s /usr/bin/fish')
     print(stdout)
+    if status != 0:
+        return False
 
     print("Now set default shell on Konsole Profile")
+    return True
 
 
-
-#install_fish()
 
 def install_fisher():
     print('Downloading fisher')
@@ -47,14 +52,19 @@ def install_fisher():
         script.write(fisher_script)
 
     stdout, stderr, status = exec_command(['fish', '-c', 'source fisher.sh; fisher install jorgebucaran/fisher'], needs_split = False, as_root = False)
+    if status != 0:
+        return False
     print(stdout, stderr, status)
     
     exec_command('rm fisher.sh')
+    return True
     
 
 def install_fish_plugins():
     for plugin in fish_plugins:
         stdout, stderr, status = exec_command(f"fish -c 'fisher install {plugin}'", as_root = False)
         print(stdout, stderr, status)
+        if status != 0:
+            return False
+    return True
 
-install_fish_plugins()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from btrfs_snapshots import make_snapshot
 from fonts import install_fonts
+from fish import install_fish, install_fisher, install_fish_plugins
 import sys
 
 snapshot = make_snapshot()
@@ -12,3 +13,18 @@ fonts = install_fonts()
 if not fonts:
     sys.exit(1)
 
+
+fish = install_fish()
+
+if not fish:
+    sys.exit(1)
+
+fisher = install_fisher()
+
+if not fisher:
+    sys.exit(1)
+
+fish_plugins = install_fish_plugins()
+
+if not fish_plugins:
+    sys.exit(1)
